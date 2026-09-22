c9af6a8fd53cb82d01fe031d83113e84c5ea890c
devlog <devlog1337@gmail.com>
2026-09-22 12:49:13 +0900

    refactor: clean selected Howly Dog yield cache sample

--- Assets/HowlyDog_SelectedCode/YieldCache.cs
+++ Assets/HowlyDog_SelectedCode/YieldCache.cs
@@ -1,47 +1,25 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Howly Dog 실제 프로젝트에서 사용 중인 Coroutine YieldInstruction 캐시 코드.
-/// 개인 프로젝트 코드 중 공개 가능한 파일만 선별하여 공개합니다.
+/// Howly Dog에서 사용한 Coroutine GC 절감 아이디어를 바탕으로,
+/// 공개 포트폴리오용으로 불필요한 delegate 캐싱을 제거해 정리한 버전입니다.
 /// </summary>
 internal static class YieldCache
 {
-    public static readonly WaitForEndOfFrame waitForEndOfFrame = new WaitForEndOfFrame();
-    public static readonly WaitForFixedUpdate waitForFixedUpdate = new WaitForFixedUpdate();
+    public static readonly WaitForEndOfFrame WaitForEndOfFrame = new WaitForEndOfFrame();
+    public static readonly WaitForFixedUpdate WaitForFixedUpdate = new WaitForFixedUpdate();
 
-    private static readonly Dictionary<float, WaitForSeconds> waitForSeconds = new Dictionary<float, WaitForSeconds>();
-    private static readonly Dictionary<Func<bool>, WaitUntil> waitUtils = new Dictionary<Func<bool>, WaitUntil>();
-    private static readonly Dictionary<Func<bool>, WaitWhile> waitWhile = new Dictionary<Func<bool>, WaitWhile>();
+    private static readonly Dictionary<float, WaitForSeconds> WaitForSecondsCache
+        = new Dictionary<float, WaitForSeconds>();
 
     public static WaitForSeconds WaitForSeconds(float seconds)
     {
-        WaitForSeconds wait;
+        if (WaitForSecondsCache.TryGetValue(seconds, out var wait))
+            return wait;
 
-        if (!waitForSeconds.TryGetValue(seconds, out wait))
-            waitForSeconds.Add(seconds, wait = new WaitForSeconds(seconds));
-
-        return wait;
-    }
-
-    public static WaitUntil WaitUntil(Func<bool> condition)
-    {
-        WaitUntil wait;
-
-        if (!waitUtils.TryGetValue(condition, out wait))
-            waitUtils.Add(condition, wait = new WaitUntil(condition));
-
-        return wait;
-    }
-
-    public static WaitWhile WaitWhile(Func<bool> condition)
-    {
-        WaitWhile wait;
-
-        if (!waitWhile.TryGetValue(condition, out wait))
-            waitWhile.Add(condition, wait = new WaitWhile(condition));
-
+        wait = new WaitForSeconds(seconds);
+        WaitForSecondsCache.Add(seconds, wait);
         return wait;
     }
 }
